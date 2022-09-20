0c9e4cbefe03f0c7cb7315f89389aaddb62404dd
Sami Ciocan <77675027+ittaMeSem@users.noreply.github.com>
2022-09-20 16:44:00 +0300

    new commit

---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Song = ({ currentSong }) => {
+  return (
+    <div className="song-container">
+      <img src={currentSong.cover} alt="song cover" />
+      <h1>{currentSong.name}</h1>
+      <h3>{currentSong.artist}</h3>
+    </div>
+  );
+};
+
+export default Song;
